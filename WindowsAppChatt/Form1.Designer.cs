97632b17d147e5a811e7c086695b3873c5181fbf
André Figueira <andre_figueira@lisboa.local>
2018-10-16 15:59:20 +0100

    Add project files.

---
+++ WindowsAppChatt/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace WindowsAppChatt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelUsername = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonSendMsg = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // labelUsername
+            // 
+            this.labelUsername.AutoSize = true;
+            this.labelUsername.Location = new System.Drawing.Point(56, 61);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(32, 13);
+            this.labelUsername.TabIndex = 6;
+            this.labelUsername.Text = "User:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(59, 77);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(59, 103);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(75, 23);
+            this.buttonLogin.TabIndex = 7;
+            this.buttonLogin.Text = "Connect";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(59, 175);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // buttonSendMsg
+            // 
+            this.buttonSendMsg.Location = new System.Drawing.Point(165, 175);
+            this.buttonSendMsg.Name = "buttonSendMsg";
+            this.buttonSendMsg.Size = new System.Drawing.Size(96, 23);
+            this.buttonSendMsg.TabIndex = 3;
+            this.buttonSendMsg.Text = "Send";
+            this.buttonSendMsg.UseVisualStyleBackColor = true;
+            this.buttonSendMsg.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(186, 103);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Disconnect";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(301, 45);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(384, 290);
+            this.listBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(709, 352);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.labelUsername);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonSendMsg);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button buttonSendMsg;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label labelUsername;
+        private System.Windows.Forms.Button buttonLogin;
+    }
+}
+
---
+++ WindowsAppChatt/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using WebSocketSharp;
+
+namespace WindowsAppChatt
+{
+    public partial class Form1 : Form
+    {
+        private WebSocket client;
+        private string host = "ws://127.0.0.1:8080/chat";
+        private string userLogged;
+        private bool conn = false;
+        private string path_string = @"C:\Users\andre_figueira\Desktop\logChat.txt"; // @"C:\Users\andre_figueira\Desktop\logChat.txt"; 
+        private StreamReader sr;
+        private string queryStringLastMessage;
+        private DateTime time;
+        private bool init;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+       //LOGIN  AND CONNECT
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+            if (!string.IsNullOrEmpty(textBox2.Text) && !conn)
+            {
+                userLogged = textBox2.Text;
+                var queryString = "?name="+ userLogged;
+
+                if (!init)
+                {
+                    string[] readHistory = File.ReadAllLines(path_string);
+                    foreach (var line in readHistory)
+                    {
+                        listBox1.Items.Add(line);
+                        queryStringLastMessage = line;
+                    }
+                    init = true;
+
+                }
+
+                client = new WebSocket(host + queryString);
+
+                //OnMessage
+                client.OnMessage += (ss, ee) => Method1(ee);
+
+                if (client.ReadyState == WebSocketState.Connecting)
+                {
+                    textBox2.Text = null;
+
+                    
+
+                    client.Connect();
+                    conn = true;
+                }
+            }
+        }
+
+        //DISCONNECT
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            //Save the time when the disconnect button was pressed
+            if (conn)
+            {
+                time = DateTime.Now;
+
+                var tex = $"{time} : {userLogged} :> Has disconnected!"+Environment.NewLine;
+                File.AppendAllText(path_string, tex);
+
+                listBox1.Items.Add(tex);
+
+                //client.Send("has disconnected!");
+                //Thread.Sleep(100);
+                client.Close();
+                userLogged = "";
+                conn = false;
+            }
+        }
+
+        //SEND
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            if (conn)//client.ReadyState == WebSocketState.Open
+            {
+                var content = textBox1.Text;
+                textBox1.Text = null;
+                if (!string.IsNullOrEmpty(content))
+                    client.Send(content);
+            }
+        }
+
+        //This should be a class level declaration
+        delegate void myDelegate(MessageEventArgs e);
+
+        private void Method1(MessageEventArgs e)
+        {
+            //We need to handle CrossThred            
+            if (listBox1.InvokeRequired)
+            {
+                //Through delegate firing the method "Method1"
+                myDelegate md = new myDelegate(Method1);
+                //Calling Invoke method to invoke the delegate and consequenty invoke Method1
+                this.Invoke(md, new object[] { e });
+            }
+            //No need to handle CrossThread            
+            else
+            {
+            //Save on the file the history chat
+
+                //sw = new StreamWriter(path_string);
+
+                var tex = e.Data + Environment.NewLine;
+                File.AppendAllText(path_string, tex);
+
+                listBox1.Items.Add(e.Data);
+                
+            }
+
+        }
+    }
+}
